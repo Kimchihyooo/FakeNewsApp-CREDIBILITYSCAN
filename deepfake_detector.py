c018413692c16d6cb79b1c9d62ffab7351d4c55f
Josh Melvin Lazaro <joshlazaro1112@gmail.com>
2026-01-10 12:24:10 +0800

    syntax keras

--- deepfake_detector.py
+++ deepfake_detector.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e43135f9edaa0d44d99f7438cbdcdad2e00320528a94f5545eec580a7e6fdb7
-size 5124
+oid sha256:e62a20270f06c5e92e33b24907b56a01d1ebb3e7203fbf5579bec4ed3fd32f00
+size 5113
